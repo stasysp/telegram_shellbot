cf0e5b46fd7f9adfb766e01c8c1ae16b972fced6
Anastasiya Popova <anastasiya.popova@xperience.ai>
2019-07-26 16:48:54 +0300

    fixes

--- run_bot.py
+++ run_bot.py
@@ -6,7 +6,7 @@ import json
 
 
 #apihelper.proxy = {'https': 'socks5://210929988:OOpkUQD1@orbtl.s5.opennetwork.cc:999'}
-apihelper.proxy = {'https': 'socks5://telegram.vpn99.net:55655'}
+#apihelper.proxy = {'https': 'socks5://telegram.vpn99.net:55655'}
 
 with open('config.json', 'r') as conf:
      conf = json.load(conf)
@@ -14,20 +14,18 @@ token = conf['token']
 user_id = conf['id']
 
 bot = telebot.TeleBot(token)
-print('lalala')
 
 @bot.message_handler(content_types=["text"])
 def main(message):
    if (user_id == message.chat.id):
       comand = message.text  
       try: 
-         bot.send_message(message.chat.id, comand) #check_output(comand, shell = True))
+         bot.send_message(message.chat.id, check_output(comand, shell = True))
       except:
          bot.send_message(message.chat.id, "Invalid input")
 if __name__ == '__main__':
-    #while True:
-    #    try:
-    bot.polling(none_stop=True, timeout=123)
-    #    except:
-    #        pass
-    #        #time.sleep(10)
+    while True:
+        try:
+            bot.polling(none_stop=True, timeout=123)
+        except:
+            time.sleep(10)
